f0c75558d6deb022ee32388e3b8046c78d8e91d5
Ei Phu Eain <eiphueain@gmail.com>
2025-08-17 18:25:39 +0200

    issue #5 adjust cat movement

--- scenes/cat.gd
+++ scenes/cat.gd
@@ -24,7 +24,7 @@ func _physics_process(delta):
 		if velocity.y > MAX_VEL:
 			velocity.y = MAX_VEL
 		if flying:
-			set_rotation(deg_to_rad(velocity.y * 0.05))
+			set_rotation(deg_to_rad(velocity.y * 0.01))
 			$AnimatedSprite2D.play()
 		elif falling:
 			set_rotation(PI/2)
